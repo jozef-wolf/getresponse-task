59137b5654b18c8969913162f5f5b0d2ccb116bc
jozef-wolf <jozefwolf@o2.pl>
2021-03-30 22:44:20 +0200

    Navbar structure

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Dashboard</li>
+        <li>Recipies</li>
+        <li>Challange</li>
+        <li>avatar</li>
+        <li>Olivia Wilde</li>
+        <li>arrow</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
