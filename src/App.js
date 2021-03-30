e488d8d54a22ae55f54d78ef031e271eb01922c8
jozef-wolf <jozefwolf@o2.pl>
2021-03-31 01:24:13 +0200

    table wip

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Navbar from "./components/Navbar/Navbar";
 import Section from "./components/Section/Section";
+import Table from "./components/Table/Table";
 import Aside from "./components/aside/Aside";
+
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Section />
+      <Table />
       <Aside />
     </div>
   );
---
+++ src/components/Table/Table.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+
+const table = () => {
+  return (
+    <div>
+      <div className="table-column">
+        <div className="table-row">time</div>
+        <div className="table-row">6:00 AM</div>
+        <div className="table-row">9:00 AM</div>
+        <div className="table-row">12:00 AM</div>
+        <div className="table-row">3:00 AM</div>
+        <div className="table-row">6:00 AM</div>
+        <div className="table-row"></div>
+        <div className="table-row">Workout</div>
+      </div>
+      <div className="table-column">
+        <div className="table-row">Day 64</div>
+        <div className="table-row"></div>
+        <div className="table-row"></div>
+        <div className="table-row"></div>
+        <div className="table-row"></div>
+        <div className="table-row"></div>
+        <div className="table-row"></div>
+        <div className="table-row"></div>
+      </div>
+      <div className="table-column">
+        <div className="table-row">Day 65</div>
+        <div className="table-row"></div>
+        <div className="table-row"></div>
+        <div className="table-row"></div>
+        <div className="table-row"></div>
+        <div className="table-row"></div>
+        <div className="table-row"></div>
+        <div className="table-row"></div>
+      </div>
+      <div className="table-column">
+        <div className="table-row">Day 66</div>
+        <div className="table-row"></div>
+        <div className="table-row"></div>
+        <div className="table-row"></div>
+        <div className="table-row"></div>
+        <div className="table-row"></div>
+        <div className="table-row"></div>
+        <div className="table-row"></div>
+      </div>
+      <div className="table-column">
+        <div className="table-row">Day 67</div>
+        <div className="table-row"></div>
+        <div className="table-row"></div>
+        <div className="table-row"></div>
+        <div className="table-row"></div>
+        <div className="table-row"></div>
+        <div className="table-row"></div>
+        <div className="table-row"></div>
+      </div>
+      <div className="table-column">
+        <div className="table-row">Day 68</div>
+        <div className="table-row"></div>
+        <div className="table-row"></div>
+        <div className="table-row"></div>
+        <div className="table-row"></div>
+        <div className="table-row"></div>
+        <div className="table-row"></div>
+        <div className="table-row"></div>
+      </div>
+      <div className="table-column">
+        <div className="table-row">Day 69</div>
+        <div className="table-row"></div>
+        <div className="table-row"></div>
+        <div className="table-row"></div>
+        <div className="table-row"></div>
+        <div className="table-row"></div>
+        <div className="table-row"></div>
+        <div className="table-row"></div>
+      </div>
+      <div className="table-column">
+        <div className="table-row">Day 70</div>
+        <div className="table-row"></div>
+        <div className="table-row"></div>
+        <div className="table-row"></div>
+        <div className="table-row"></div>
+        <div className="table-row"></div>
+        <div className="table-row"></div>
+        <div className="table-row"></div>
+      </div>
+    </div>
+  );
+};
+
+export default table;
