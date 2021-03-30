b8f71134352911507d3d256102abca523d9a540e
jozef-wolf <jozefwolf@o2.pl>
2021-03-30 23:47:06 +0200

    Aside structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar/Navbar";
 import Section from "./components/Section/Section";
+import Aside from "./components/aside/Aside";
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Section />
+      <Aside />
     </div>
   );
 }
---
+++ src/components/aside/Aside.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+const Aside = () => {
+  return (
+    <aside>
+      <div>
+        <h2>Running out of products?</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consect turadipiscing elit. Integer
+          aliquet.
+        </p>
+        <span>
+          <img src="" alt="" />
+        </span>
+      </div>
+      <div>
+        <h2>Bod•e Trainer in your pocket</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consect turadipiscing elit. Integer
+          aliquet.
+        </p>
+        <span>
+          <img src="" alt="" />
+        </span>
+        <span>
+          <img src="" alt="" />
+        </span>
+      </div>
+      <div>
+        <h2>Frequently Asked Questions</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consect turadipiscing elit. Integer
+          aliquet.
+        </p>
+        <span>
+          <img src="" alt="" />
+        </span>
+      </div>
+    </aside>
+  );
+};
+
+export default Aside;
