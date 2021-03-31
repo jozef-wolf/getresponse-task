80ec9f296a7d7e86ca5826dbec1d2dec9e378b0e
jozef-wolf <jozefwolf@o2.pl>
2021-03-31 02:31:36 +0200

    table rows wip

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,89 +1,103 @@
 import React from "react";
-
+import "./style.css";
 const table = () => {
   return (
-    <div>
-      <div className="table-column">
-        <div className="table-row">time</div>
-        <div className="table-row">6:00 AM</div>
-        <div className="table-row">9:00 AM</div>
-        <div className="table-row">12:00 AM</div>
-        <div className="table-row">3:00 AM</div>
-        <div className="table-row">6:00 AM</div>
-        <div className="table-row"></div>
-        <div className="table-row">Workout</div>
-      </div>
-      <div className="table-column">
-        <div className="table-row">Day 64</div>
-        <div className="table-row"></div>
-        <div className="table-row"></div>
-        <div className="table-row"></div>
-        <div className="table-row"></div>
-        <div className="table-row"></div>
-        <div className="table-row"></div>
-        <div className="table-row"></div>
-      </div>
-      <div className="table-column">
-        <div className="table-row">Day 65</div>
-        <div className="table-row"></div>
-        <div className="table-row"></div>
-        <div className="table-row"></div>
-        <div className="table-row"></div>
-        <div className="table-row"></div>
-        <div className="table-row"></div>
-        <div className="table-row"></div>
-      </div>
-      <div className="table-column">
-        <div className="table-row">Day 66</div>
-        <div className="table-row"></div>
-        <div className="table-row"></div>
-        <div className="table-row"></div>
-        <div className="table-row"></div>
-        <div className="table-row"></div>
-        <div className="table-row"></div>
-        <div className="table-row"></div>
-      </div>
-      <div className="table-column">
-        <div className="table-row">Day 67</div>
-        <div className="table-row"></div>
-        <div className="table-row"></div>
-        <div className="table-row"></div>
-        <div className="table-row"></div>
-        <div className="table-row"></div>
-        <div className="table-row"></div>
-        <div className="table-row"></div>
-      </div>
-      <div className="table-column">
-        <div className="table-row">Day 68</div>
-        <div className="table-row"></div>
-        <div className="table-row"></div>
-        <div className="table-row"></div>
-        <div className="table-row"></div>
-        <div className="table-row"></div>
-        <div className="table-row"></div>
-        <div className="table-row"></div>
-      </div>
-      <div className="table-column">
-        <div className="table-row">Day 69</div>
-        <div className="table-row"></div>
-        <div className="table-row"></div>
-        <div className="table-row"></div>
-        <div className="table-row"></div>
-        <div className="table-row"></div>
-        <div className="table-row"></div>
-        <div className="table-row"></div>
-      </div>
-      <div className="table-column">
-        <div className="table-row">Day 70</div>
-        <div className="table-row"></div>
-        <div className="table-row"></div>
-        <div className="table-row"></div>
-        <div className="table-row"></div>
-        <div className="table-row"></div>
-        <div className="table-row"></div>
-        <div className="table-row"></div>
-      </div>
-    </div>
+    <table>
+      <thead>
+        <tr>
+          <th></th>
+          <th className="header-row">DAY 64</th>
+          <th className="header-row">DAY 65</th>
+          <th className="header-row">DAY 66</th>
+          <th className="header-row">DAY 67</th>
+          <th className="header-row">DAY 68</th>
+          <th className="header-row">DAY 69</th>
+          <th className="header-row">DAY 70</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td></td>
+          <td className="table-row">Cell</td>
+          <td className="table-row">Cell</td>
+          <td className="table-row">Cell</td>
+          <td className="table-row">Cell</td>
+          <td className="table-row">Cell</td>
+          <td className="table-row">Cell</td>
+          <td rowSpan="6">dupa</td>
+        </tr>
+        <tr>
+          <td>6:00 AM</td>
+          <td className="table-row">Cell</td>
+          <td className="table-row">Cell</td>
+          <td className="table-row">Cell</td>
+          <td className="table-row">Cell</td>
+          <td className="table-row">Cell</td>
+          <td className="table-row">Cell</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>9:00 AM</td>
+          <td className="table-row">Cell</td>
+          <td className="table-row">Cell</td>
+          <td className="table-row">Cell</td>
+          <td className="table-row">Cell</td>
+          <td className="table-row">Cell</td>
+          <td className="table-row">Cell</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>12:00 AM</td>
+          <td className="table-row">Cell</td>
+          <td className="table-row">Cell</td>
+          <td className="table-row">Cell</td>
+          <td className="table-row">Cell</td>
+          <td className="table-row">Cell</td>
+          <td className="table-row">Cell</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>3:00 AM</td>
+          <td className="table-row">Cell</td>
+          <td className="table-row">Cell</td>
+          <td className="table-row">Cell</td>
+          <td className="table-row">Cell</td>
+          <td className="table-row">Cell</td>
+          <td className="table-row">Cell</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>6:00 AM</td>
+          <td className="table-row">Cell</td>
+          <td className="table-row">Cell</td>
+          <td className="table-row">Cell</td>
+          <td className="table-row">Cell</td>
+          <td className="table-row">Cell</td>
+          <td className="table-row">Cell</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td className="carb-row">Cell</td>
+          <td className="carb-row">Cell</td>
+          <td className="carb-row">Cell</td>
+          <td className="carb-row">Cell</td>
+          <td className="carb-row">Cell</td>
+          <td className="carb-row">Cell</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td rowSpan="2">Workout</td>
+          <td className="bottom-row">Cell</td>
+          <td className="bottom-row">Cell</td>
+          <td className="bottom-row">Cell</td>
+          <td className="bottom-row">Cell</td>
+          <td className="bottom-row">Cell</td>
+          <td className="bottom-row">Cell</td>
+          <td rowSpan="2">Print</td>
+        </tr>
+      </tbody>
+    </table>
   );
 };
 
