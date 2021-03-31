5c6ed273e790e0eec63385a22235bcf17d7efc2a
jozef-wolf <jozefwolf@o2.pl>
2021-03-31 20:16:47 +0200

    table tr th wip

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,119 +1,20 @@
 import React from "react";
 import "./style.css";
-import { ReactComponent as ImportedSVG } from "../imgs/print.svg";
-import { ReactComponent as Check } from "../imgs/check.svg";
-import Dumbbell from "../imgs/Dumbbell2x.png";
-import DumbbellGrey from "../imgs/DumbbellGrey2x.png";
-import Rectangle from "../imgs/rectangle2x.png";
-import Happy from "../imgs/happy2x.png";
+// import { ReactComponent as ImportedSVG } from "../imgs/print.svg";
+// import { ReactComponent as Check } from "../imgs/check.svg";
+// import Dumbbell from "../imgs/Dumbbell2x.png";
+// import DumbbellGrey from "../imgs/DumbbellGrey2x.png";
+// import Rectangle from "../imgs/rectangle2x.png";
+// // import Happy from "../imgs/happy2x.png";
 
 const Table = ({ meals }) => {
   return (
     <table>
-      <thead>
+      {meals.map((meal) => (
         <tr>
-          <th></th>
-          <th className="header-row title">DAY 64</th>
-          <th className="header-row title">DAY 65</th>
-          <th className="header-row title">DAY 66</th>
-          <th className="header-row title">DAY 67</th>
-          <th className="header-row title">DAY 68</th>
-          <th className="header-row title">DAY 69</th>
-          <th className="header-row title">DAY 70</th>
+          <th>{meal.id}</th>
         </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td className="time-column">6:00 AM</td>
-          <td className="table-row"></td>
-          <td className="table-row"></td>
-          <td className="table-row"></td>
-          <td className="table-row"></td>
-          <td className="table-row"></td>
-          <td className="table-row"></td>
-          <td rowSpan="5" className="vertical-row">
-            <div className="vertical">
-              <img className="happy-logo" src={Happy} alt="" />
-              GUILTY-FREE DAY
-            </div>
-          </td>
-        </tr>
-        <tr>
-          <td className="time-column">9:00 AM</td>
-          <td className="table-row"></td>
-          <td className="table-row"></td>
-          <td className="table-row"></td>
-          <td className="table-row"></td>
-          <td className="table-row"></td>
-          <td className="table-row"></td>
-        </tr>
-        <tr>
-          <td className="time-column">12:00 AM</td>
-          <td className="table-row"></td>
-          <td className="table-row"></td>
-          <td className="table-row"></td>
-          <td className="table-row"></td>
-          <td className="table-row"></td>
-          <td className="table-row"></td>
-        </tr>
-        <tr>
-          <td className="time-column">3:00 AM</td>
-          <td className="table-row"></td>
-          <td className="table-row"></td>
-          <td className="table-row"></td>
-          <td className="table-row"></td>
-          <td className="table-row"></td>
-          <td className="table-row"></td>
-        </tr>
-        <tr>
-          <td className="time-column">6:00 AM</td>
-          <td className="table-row"></td>
-          <td className="table-row"></td>
-          <td className="table-row"></td>
-          <td className="table-row"></td>
-          <td className="table-row"></td>
-          <td className="table-row"></td>
-        </tr>
-        <tr>
-          <td className="carb-row"></td>
-          <td className="carb-row"></td>
-          <td className="carb-row"></td>
-          <td className="carb-row"></td>
-          <td className="carb-row"></td>
-          <td className="carb-row"></td>
-          <td className="carb-row"></td>
-          <td rowSpan="2" className="print-row">
-            <ImportedSVG />
-            Print
-          </td>
-        </tr>
-        <tr>
-          <td className="bottom-row">
-            Workout
-            <img src={Rectangle} alt="" />
-          </td>
-          <td className="bottom-row">
-            <img src={Dumbbell} alt="" />
-            <Check />
-          </td>
-          <td className="bottom-row">
-            <img src={Dumbbell} alt="" />
-            <Check />
-          </td>
-          <td className="bottom-row">
-            <img src={DumbbellGrey} alt="" />
-          </td>
-          <td className="bottom-row">
-            <img src={DumbbellGrey} alt="" />
-          </td>
-          <td className="bottom-row">
-            <img src={DumbbellGrey} alt="" />
-          </td>
-          <td className="bottom-row">
-            <img src={DumbbellGrey} alt="" />
-          </td>
-        </tr>
-      </tbody>
+      ))}
     </table>
   );
 };
