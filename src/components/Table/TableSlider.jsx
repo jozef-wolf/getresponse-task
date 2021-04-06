cf33960b88cf54912db3bef85bdc96a808ad431c
jozef-wolf <jozefwolf@o2.pl>
2021-04-06 17:12:54 +0200

    slider corrections

--- src/components/Table/TableSlider.jsx
+++ src/components/Table/TableSlider.jsx
@@ -6,34 +6,76 @@ import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
 const StyledSlider = styled.div`
   display: flex;
   justify-content: space-between;
-  @media (max-width: 768px) {
-    margin-bottom: 40px;
-  }
+  align-items: center;
+  position: relative;
   @media (min-width: 769px) {
     display: none;
   }
 
-  .day__card {
-    box-shadow: 4px 2px 2px 5px #dadada;
-    border-radius: 40px;
-    height: 600px;
+  .box {
     display: flex;
-    flex-direction: column;
+    flex-direction: row;
+    justify-content: center;
+    .week {
+      opacity: 0;
+      transition-duration: 1s ease;
+    }
+    .week.active {
+      opacity: 1;
+      transition-duration: 1s;
+    }
+    &__card {
+      box-shadow: 4px 2px 8px 8px #c2c2c2;
+      border-radius: 40px;
+      height: 500px;
+      display: flex;
+      flex-direction: column;
+      > div {
+        text-align: center;
+      }
+      &--data {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        font-size: 50px;
+        color: #adbb0c;
+      }
+      &--day {
+        display: flex;
+        flex-direction: row;
+        border-top: 1px solid #c2c2c2;
+        color: black;
+        align-items: center;
+        height: 100%;
+        .header {
+          color: #c2c2c2;
+          font-size: 14px;
+          height: 100%;
+          width: 70px;
+        }
+        .meal {
+          height: 100%;
+          width: 100%;
+          justify-content: center;
+          display: flex;
+          align-items: center;
+          font-size: 16px;
+        }
+      }
+      &--workout {
+        border-top: 1px solid #c2c2c2;
+        p {
+          font-size: 20px;
+          font-weight: bold;
+        }
 
-    > div {
-      border-bottom: 2px solid black;
-      text-align: center;
+        img {
+          height: 25px;
+        }
+      }
     }
   }
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: relative;
-  button:hover {
-    background-color: #ff801a;
-    transition: ease-in 0.5s;
-    transform: scale(1.2);
-  }
+
   button {
     width: 40px;
     margin: 2px;
@@ -42,57 +84,14 @@ const StyledSlider = styled.div`
     display: flex;
     align-items: center;
     cursor: pointer;
-    background-color: #ff801a;
+    background-color: #adbb0c;
     border: none;
     outline: none;
-  }
-  .box {
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    .box__week {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      font-size: 50px;
-      color: #ff801a;
-      line-height: 1;
+    &:hover {
+      background-color: #adbb0c;
+      transition: ease-out 0.6s;
+      transform: scale(1.2);
     }
-    .box__meal {
-      display: flex;
-      flex-direction: row;
-
-      color: black;
-      align-items: center;
-      height: 100%;
-      .header {
-        border-right: 1px solid black;
-        background-color: #dadada;
-        font-size: 14px;
-        height: 100%;
-        width: 70px;
-      }
-      .meal {
-        height: 100%;
-        width: 100%;
-        text-align: center;
-        font-size: 18px;
-      }
-    }
-    .box__workout {
-      font-size: 25px;
-      .dumbbell {
-        height: 25px;
-      }
-    }
-  }
-  .week {
-    opacity: 0;
-    transition-duration: 1s ease;
-  }
-  .week.active {
-    opacity: 1;
-    transition-duration: 1s;
   }
 `;
 
@@ -119,39 +118,35 @@ const TableSlider = ({ meals, rowHeaders }) => {
           >
             {index === current && (
               <>
-                <div className="day__card">
-                  <div className="box__week">DAY {meal.id}</div>
-                  <div className="box__meal">
+                <div className="box__card">
+                  <div className="box__card--data">DAY {meal.id}</div>
+                  <div className="box__card--day">
                     <div className="header">{rowHeaders[1]}</div>
                     <div className="meal">{meal.sixAM}</div>
                   </div>
-                  <div className="box__meal">
+                  <div className="box__card--day">
                     <div className="header">{rowHeaders[2]}</div>
                     <div className="meal">{meal.nineAM}</div>
                   </div>
-                  <div className="box__meal">
+                  <div className="box__card--day">
                     <div className="header">{rowHeaders[3]}</div>
                     <div className="meal">{meal.twelveAM}</div>
                   </div>
-                  <div className="box__meal">
+                  <div className="box__card--day">
                     <div className="header">{rowHeaders[4]}</div>
                     <div className="meal">{meal.threePM}</div>
                   </div>
-                  <div className="box__meal">
+                  <div className="box__card--day">
                     <div className="header">{rowHeaders[5]}</div>
                     <div className="meal">{meal.sixPM}</div>
                   </div>
-                  <div className="box__meal">
-                    <div className="header">{rowHeaders[1]}</div>
+                  <div className="box__card--day">
+                    <div className="header">{rowHeaders[6]}</div>
                     <div className="meal">{meal.Carb}</div>
                   </div>
-                  <div className="box__workout">
-                    <div> WORKOUT</div>
-                    <img
-                      className="dumbbell"
-                      src={DumbbellGrey}
-                      alt="dumbbell"
-                    />
+                  <div className="box__card--workout">
+                    <p>WORKOUT</p>
+                    <img src={DumbbellGrey} alt="dumbbell" />
                   </div>
                 </div>
               </>
