5aa6aaf9f7867d4735d1e0008cdf88e9823e359c
jozef-wolf <jozefwolf@o2.pl>
2021-04-07 13:11:37 +0200

    aside responsivness

--- src/components/aside/Aside.jsx
+++ src/components/aside/Aside.jsx
@@ -7,67 +7,108 @@ import Android from "../imgs/android2x.png";
 const StyledAside = styled.aside`
   display: flex;
   flex-direction: row;
+  flex-wrap: wrap;
   margin-top: 45px;
+  @media (max-width: 768px) {
+    justify-content: center;
+  }
+  .box__first,
+  .box__second,
+  .box__third {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    height: 190px;
+    text-align: left;
+    @media (max-width: 768px) {
+      padding: 0px !important;
+      margin: 30px;
+      height: 200px;
+      text-align: center;
+      width: 300px !important;
+      border-top: 2px solid #b8b8b8;
+    }
+  }
   h2 {
     font-size: 28.5px;
     height: 55px;
+    line-height: 1;
+    letter-spacing: -0.86px;
+    text-align: left;
+    color: #828282;
   }
   > p {
     font-family: Arial;
     font-size: 12.5px;
     width: 237px;
     height: 35px;
+    line-height: 1.42;
+    letter-spacing: -0.47px;
+    text-align: left;
+    color: #828282;
   }
-  span:hover{
+  span:hover {
     cursor: pointer;
   }
-  span{
+  span {
     display: flex;
     justify-content: space-around;
     align-items: center;
     border-radius: 5px;
     border: solid 1px #b8b8b8;
-    img:first-child{
+    img:first-child {
       width: 16px;
       height: 19px;
     }
-  
   }
-  .arrow{
+  .arrow {
     width: 7px;
-  height: 13px;
+    height: 13px;
   }
-  .box__first > span{
+  .box__first {
+    width: 307px;
+    > span {
       width: 106px;
       height: 33px;
+    }
   }
-  .box__second{
+
+  .box__second {
     height: 190px;
     text-align: left;
-    width: 366px;
+    width: 316px;
     border-left: 1px solid black;
     border-right: 1px solid black;
     padding-left: 48px;
-    span:first-child{
-    width: 95px;
-    height: 33px;
+    @media (max-width: 768px) {
+      border-left: none;
+      border-right: none;
+      height: 240px;
     }
-    span:last-child{
+    > p {
+      width: 237px;
+      height: 35px;
+    }
+    div {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-around;
+    }
+    span:first-child {
+      width: 95px;
+      height: 33px;
+    }
+    span:last-child {
       width: 125px;
       height: 33px;
     }
   }
-  .box__third{
+  .box__third {
+    width: 275px;
     padding-left: 39px;
-    span{
+    span {
       width: 126px;
       height: 33px;
-    }
-  }
-  .box__first,
-  .box___third {
-    height: 190px;
-    text-align: left;
     }
   }
 `;
